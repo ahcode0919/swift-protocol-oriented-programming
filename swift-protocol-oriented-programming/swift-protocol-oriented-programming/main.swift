72e8598f8ad2efbab969d85cd53610185d5f8854
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2017-11-02 00:31:04 -0400

    Initial Project

---
+++ swift-protocol-oriented-programming/swift-protocol-oriented-programming/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-protocol-oriented-programming
+//
+//  Created by Aaron Hinton on 11/2/17.
+//  Copyright © 2017 No Name Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
